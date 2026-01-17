0c6bf762d545c93b02880cab6856607c09bef830
James <73779218+Somebody-somewher@users.noreply.github.com>
2026-01-17 23:29:29 +0800

    ChatBotBob.java: Add simple Echo functionality. Update Greeting Styling

--- src/main/java/ChatBotBob.java
+++ src/main/java/ChatBotBob.java
@@ -1,14 +1,40 @@
+import java.util.Scanner;
+
 public class ChatBotBob {
-    private static String welcomeString = """
-            ____________________________________________________________
-             Hello! I'm Bob :D
-             What can I do for you?
-            ____________________________________________________________
-             Bye. Hope to see you again soon!
-            ____________________________________________________________
+    private static final String SEGMENT_SEPARATOR = """
+            ════════════════════════════ ✧ ✧ ✧ ════════════════════════════
             """;
 
+    private static final String WELCOME_STRING = SEGMENT_SEPARATOR + """
+             Wazzup! I'm Bob. ChatBot Bob :D
+             What can I do for you?
+            """;
+
+    private static final String GOODBYE_STRING = """
+            Buh-Bye!
+            """ ;
+
+    private static final String END_COMMAND_STRING = "bye";
+
     public static void main(String[] args) {
-        System.out.println(welcomeString);
+
+        echo(WELCOME_STRING);
+
+        // Read user input
+        Scanner reader = new Scanner(System.in);
+        String command = reader.nextLine();
+        System.out.print(SEGMENT_SEPARATOR);
+
+        if (command.equals(END_COMMAND_STRING)) {
+            echo(GOODBYE_STRING);
+            return;
+        }
+        echo(command + "\n");
     }
+
+    private static void echo(String echoString) {
+        System.out.print(echoString);
+        System.out.print(SEGMENT_SEPARATOR);
+    }
+
 }
