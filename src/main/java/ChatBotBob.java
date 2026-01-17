64213283f7d0c473bacbbea44e7eace5ed5f8b6e
James <73779218+Somebody-somewher@users.noreply.github.com>
2026-01-17 20:58:23 +0800

    Duke.java: Change name and greeting to ChatBotBob.java

---
+++ src/main/java/ChatBotBob.java
@@ -0,0 +1,14 @@
+public class ChatBotBob {
+    private static String welcomeString = """
+            ____________________________________________________________
+             Hello! I'm Bob :D
+             What can I do for you?
+            ____________________________________________________________
+             Bye. Hope to see you again soon!
+            ____________________________________________________________
+            """;
+
+    public static void main(String[] args) {
+        System.out.println(welcomeString);
+    }
+}
