c326f2ae8eb65d7e060437a6ba09e8ee11fbf7a9
James <73779218+Somebody-somewher@users.noreply.github.com>
2026-02-01 21:18:36 +0800

    Modify Parser to use a HashMap for efficiency

--- src/main/java/chatbotbob/service/Parser.java
+++ src/main/java/chatbotbob/service/Parser.java
@@ -1,7 +1,12 @@
 package chatbotbob.service;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import chatbotbob.command.Command;
+
 
 /**
  * Represents an abstraction that takes in User Input, processes it,
@@ -9,7 +14,7 @@ import chatbotbob.command.Command;
  */
 public class Parser implements ParserInterface {
     /** List of Commands that will be used by the ChatBot */
-    private List<Command> commands;
+    private Map<String, Command> commandMapping;
 
     /**
      * Loads commands into Parser and creates a Parser Instance.
@@ -19,7 +24,10 @@ public class Parser implements ParserInterface {
      * @param commands List of commands for Parser to accept
      */
     public Parser(List<Command> commands) {
-        this.commands = commands;
+        commandMapping = new HashMap<>();
+        for (Command c : commands) {
+            commandMapping.put(c.getCmdPhrase(), c);
+        }
     }
 
     /**
@@ -33,13 +41,14 @@ public class Parser implements ParserInterface {
         String[] userInputStringArr = userInputString.split(" ");
 
         // Go through every single command to see if any command matches
-        for (Command c : commands) {
-            try {
+        try {
+            Command c = commandMapping.get(userInputStringArr[0]);
+            if (!Objects.isNull(c)) {
                 c.executeOnMatch(userInputStringArr, ui);
                 return;
-            } catch (Command.CommandInvalidArgumentException e) {
-                ui.printText(e.getMessage());
             }
+        } catch (Command.CommandInvalidArgumentException e) {
+            ui.printText(e.getMessage());
         }
         ui.printSeparator();
     }
